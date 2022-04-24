3fc3fdc3a36a1025088f19250848c10fb6f64319
1xtend <kokitoki0504@gmail.com>
2022-04-24 14:12:58 +0200

    first commit

---
+++ Module-1/task-1.js
@@ -0,0 +1,53 @@
+'use strict';
+
+// Task 1
+
+const str = 'Привіт';
+console.log('Тип: ' + typeof str);
+
+const num = 123;
+console.log('Тип: ' + typeof num);
+
+const flag = true;
+console.log('Тип: ' + typeof flag);
+
+const txt = 'true';
+console.log('Тип: ' + typeof txt);
+
+// / Task 1
+
+console.log('------------------------------');
+
+// Task 2
+
+const type = 'Машина';
+console.log('Тип: ' + type);
+
+let brand = 'Toyota';
+console.log('Бренд: ' + brand);
+
+const isFast = true;
+console.log('Швидка?: ' + isFast);
+
+let width = 1.8;
+console.log('Ширина: ' + width + 'м');
+
+let height = 1.5;
+console.log('Висота: ' + height + 'м');
+
+const isNew = false;
+console.log('Нова?: ' + isNew);
+
+let maxSpeed = 160;
+console.log('Максимальна швидкість: ' + maxSpeed + 'км/ч');
+
+const minSpeed = null;
+console.log('Мінімальна швидкість: ' + minSpeed);
+
+let condition = 'Потребує заміни переднього бампера';
+console.log('Стан: ' + condition);
+
+const isExpensive = false;
+console.log('Дорого?: ' + isExpensive);
+
+// / Task 2
