9a77e6fc61a95838c622874b7558e17260402879
1xtend <kokitoki0504@gmail.com>
2022-04-29 20:54:02 +0200

    task-2

---
+++ Module-1/task-2.js
@@ -0,0 +1,96 @@
+'use strict';
+
+// Task 1
+
+// const yourName = 'Enter your name';
+// const result = prompt(yourName);
+// console.log(result);
+
+// / Task 1
+
+// Task 2
+
+// const yourAge = 24;
+// const yourFriendAge = 27;
+
+// const sumOfAges = yourAge + yourFriendAge;
+// alert('Sum of ages: ' + sumOfAges);
+
+// / Task 2
+
+// Task 3
+
+// const a = +prompt('First number');
+// const b = +prompt('Second number');
+
+// const sumOfNumbers = a + b;
+// console.log('Sum of numbers: ' + sumOfNumbers);
+
+// / Task 3
+
+// Task 4
+
+// const a1 = 5 + 3;
+// console.log('5 + 3 = ' + a1);
+
+// const a2 = 5 - 3;
+// console.log('5 - 3 = ' + a2);
+
+// const a3 = 5 * 3;
+// console.log('5 * 3 = ' + a3);
+
+// const a4 = 5 / 3;
+// console.log('5 / 3 = ' + a4);
+
+// Task 4
+
+// Task 5
+
+// const a5 = 5 % 3;
+// alert('5 % 3 = ' + a5);
+
+// const a6 = 3 % 5;
+// alert('3 % 5 = ' + a6);
+
+// const a7 = 5 + '3';
+// alert("5 + '3' = " + a7);
+
+// const a8 = '5' - 3;
+// alert("'5' - 3 = " + a8);
+
+// const a9 = 75 + 'кг';
+// alert(a9);
+
+// / Task 5
+
+// Task 6
+
+// const height = 23;
+// const width = 10;
+
+// const s = height * width;
+// console.log('Площа прямокутника: ' + s + 'см');
+
+// / Task 6
+
+// Task 7
+
+// const heightC = 10;
+// const dC = 4;
+
+// const v = Math.PI * Math.pow(dC, 2) * heightC;
+// console.log('Обсяг циліндра: ' + v);
+
+// / Task 7
+
+// Task 8
+
+// const n = 3;
+// const m = 4;
+
+// const k = Math.pow(n, 2) + Math.pow(m, 2);
+// // const k = n ** 2 + m ** 2;
+
+// console.log('Гіпотенуза: ' + k);
+
+// / Task 8
