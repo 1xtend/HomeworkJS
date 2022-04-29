73bcb6524c9fc2ef73823e0a89e57c8c19040d52
1xtend <kokitoki0504@gmail.com>
2022-04-29 20:58:46 +0200

    task-2

--- Module-1/task-2.js
+++ Module-1/task-2.js
@@ -88,7 +88,7 @@
 // const n = 3;
 // const m = 4;
 
-// const k = Math.pow(n, 2) + Math.pow(m, 2);
+// const k = Math.sqrt(Math.pow(n, 2) + Math.pow(m, 2));
 // // const k = n ** 2 + m ** 2;
 
 // console.log('Гіпотенуза: ' + k);
