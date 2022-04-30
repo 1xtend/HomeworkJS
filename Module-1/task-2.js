c7edc01426a899edc539ba6adf9eea4e52ecd973
1xtend <kokitoki0504@gmail.com>
2022-04-30 13:04:13 +0200

    task-2

--- Module-1/task-2.js
+++ Module-1/task-2.js
@@ -78,7 +78,7 @@
 // const heightC = 10;
 // const dC = 4;
 
-// const v = Math.PI * Math.pow(dC, 2) * heightC;
+// const v = Math.PI * Math.pow(dC / 2, 2) * heightC;
 // console.log('Обсяг циліндра: ' + v);
 
 // / Task 7
