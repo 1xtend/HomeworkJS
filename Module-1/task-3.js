acca6400aa609755e4230c26aa01cad97283d77f
1xtend <kokitoki0504@gmail.com>
2022-05-06 16:40:24 +0200

    task-3

---
+++ Module-1/task-3.js
@@ -0,0 +1,83 @@
+'use strict';
+
+// Task 1
+
+// const k1 = parseInt('5px');
+// const k2 = parseInt('12djd334');
+// const k3 = parseInt('12.45asdwe12');
+// const k4 = parseInt('qwqweeewq');
+
+// console.log(k1, k2, k3, k4);
+
+// / Task 1
+
+// Task 2
+
+// const a1 = parseFloat('5px');
+// const a2 = parseFloat('12djd334');
+// const a3 = parseFloat('12.45asdwe12');
+// const a4 = parseFloat('qwqweeewq');
+
+// console.log(a1, a2, a3, a4);
+
+// / Task 2
+
+// Task 3
+
+// console.log(Math.max(2, 34, 99, 3, 22, 36, 733, 18));
+// console.log(Math.min(2, 34, 99, 3, 22, 36, 733, 18));
+
+// / Task 3
+
+// Task 4
+
+// const random = Math.random() * (19 - 3) + 3;
+// console.log(random);
+
+// / Task 4
+
+// Task 5
+
+// const result = 5 + 5 + '5';
+// console.log(result);
+// console.log(typeof result);
+
+// / Task 5
+
+// Task 6
+
+// const email = 'vlad.mezhevyi@gmail.com';
+
+// console.log(email.includes('@'));
+// console.log(email.length);
+
+// / Task 6
+
+// Task 7
+
+// const w1 = 'My';
+// const w2 = 'name';
+// const w3 = 'is';
+
+// const fullName = `${w1} ${w2} ${w3} Viktor`;
+// console.log(fullName);
+
+// --------------------------
+
+// const w1 = 'My ';
+// const w2 = 'name ';
+// const w3 = 'is ';
+
+// const fullName = w1 + w2 + w3 + 'Viktor';
+// console.log(fullName);
+
+// / Task 7
+
+// Task 8
+
+// const userName = 'Олександро';
+// const payment = 300;
+
+// alert(`Дякуємо, ${userName}! До сплати ${payment} гривень`);
+
+// / Task 8
