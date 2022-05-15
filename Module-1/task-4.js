5498a17dae2834b35c1b290fdad2ad5244d331af
1xtend <kokitoki0504@gmail.com>
2022-05-15 14:52:58 +0200

    task-4

---
+++ Module-1/task-4.js
@@ -0,0 +1,102 @@
+'use strict';
+
+// Task 1
+
+// const yourNumber = prompt('Введіть число');
+
+// if (yourNumber >= 55 && yourNumber <= 99) {
+//   console.log('Число потрапляє в діапазон');
+// } else {
+//   console.log('Число не потрапляє в діапазон');
+// }
+
+// / Task 1
+
+// Task 2
+
+// const age = 55;
+// // const age = prompt('Введіть свій рік');
+
+// if (age >= 0 && age <= 16) {
+//   console.log('Вікова група - діти');
+// }
+
+// if (age >= 17 && age <= 60) {
+//   console.log('Вікова група - дорослі');
+// }
+
+// if (age >= 61 && age <= 100) {
+//   console.log('Вікова група - пенсіонери');
+// }
+
+// / Task 2
+
+// Task 3
+
+// const yourName = prompt("Введіть ваше ім'я");
+// const yourLastName = prompt('Введіть ваше прізвище');
+
+// if (yourName.length > 4 && yourLastName.length > 5) {
+//   alert(yourName.length + yourLastName.length);
+// } else {
+//   alert('Упс, щось трапилось не так');
+// }
+
+// / Task 3
+
+// Task 4
+
+// // const randomNumber = Math.round(Math.random() * 5) + 1;
+// // console.log(randomNumber);
+
+// const randomNumber = Math.random() * 5;
+
+// if (randomNumber >= 0 && randomNumber < 1) {
+//   console.log(1);
+// }
+
+// if (randomNumber >= 1 && randomNumber < 2) {
+//   console.log(2);
+// }
+
+// if (randomNumber >= 2 && randomNumber < 3) {
+//   console.log(3);
+// }
+
+// if (randomNumber >= 3 && randomNumber < 4) {
+//   console.log(4);
+// }
+
+// if (randomNumber >= 4 && randomNumber < 5) {
+//   console.log(5);
+// }
+
+// Task 4
+
+// Task 5
+
+// const lang = prompt('Виберіть мову: ua, en, ru, fr');
+
+// switch (lang) {
+//   case 'ua':
+//     console.log('Травень');
+//     break;
+
+//   case 'en':
+//     console.log('May');
+//     break;
+
+//   case 'ru':
+//     console.log('Май');
+//     break;
+
+//   case 'fr':
+//     console.log('Mai');
+//     break;
+
+//   default:
+//     console.log('Невідома мова');
+//     break;
+// }
+
+// Task 5
