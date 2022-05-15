5d6a84c1ad8668f55453df496da3da7556f41ba6
1xtend <kokitoki0504@gmail.com>
2022-05-15 15:32:25 +0200

    task-5

---
+++ Module-1/task-5.js
@@ -0,0 +1,50 @@
+'use strict';
+
+// Task 1
+
+// let i = 0;
+
+// while (i < 10) {
+//   i++;
+//   console.log(i);
+// }
+
+// / Task 1
+
+// Task 2
+
+// let i = 0;
+
+// while (i < 10) {
+//   i++;
+
+//   if (i % 2 === 0) {
+//     console.log('Число парне', i);
+//   } else {
+//     console.log('Число непарне', i);
+//   }
+// }
+
+// / Task 2
+
+// Task 3
+
+// let num = 20;
+
+// while (num < 28) {
+//   num += 1;
+//   console.log(num);
+// }
+
+// / Task 3
+
+// Task 4
+
+// let name = prompt("Введіть ім'я свого улюбленого героя");
+
+// while (name.length < 6) {
+//   console.log(name);
+//   break;
+// }
+
+// / Task 4
