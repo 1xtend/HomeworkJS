f02f22ecec436c845d89ff46f5953a4194f95eab
1xtend <kokitoki0504@gmail.com>
2022-06-08 12:08:35 +0200

    Module 2, task-1

---
+++ Module-2/task-1.js
@@ -0,0 +1,64 @@
+'use strict';
+
+// Task 1
+
+// const arr1 = [1, 5, '4', 'hello'];
+// const arr2 = [true, 2, {}, ['a'], 222];
+
+// const sumOfArrays = arr1[1] + arr2[1];
+
+// console.log(arr1[1], arr2[1]);
+// console.log(sumOfArrays);
+
+// / Task 1
+
+// Task 2
+
+// const arr1 = [1, 5, '4', 'hello'];
+
+// arr1.push(22);
+
+// console.log(arr1);
+
+// for (let i = 0; i < arr1.length; i++) {
+//   console.log(arr1[i]);
+// }
+
+// / Task 2
+
+// Task 3
+
+// const arr2 = [true, 2, {}, ['a'], 222];
+
+// for (const numOfArray2 of arr2) {
+//   console.log(typeof numOfArray2, numOfArray2);
+// }
+
+// / Task 3
+
+// Task 4
+
+// const message = 'Welcome to Ukraine!';
+
+// const messageArr = message.split('');
+
+// console.log(messageArr, messageArr.indexOf('l'));
+
+// const messageArr2 = messageArr.join('');
+
+// console.log(messageArr2);
+
+// / Task 4
+
+// Task 5
+
+// const styles = ['Джаз', 'Блюз'];
+
+// styles.push('Рок-н-ролл');
+// styles.splice(1, 1, 'Класика');
+// console.log(styles.shift(0));
+// styles.unshift('Реп', 'Реггі');
+
+// console.log(styles);
+
+// / Task 5
