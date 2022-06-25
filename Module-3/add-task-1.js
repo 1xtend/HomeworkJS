700b2f1f508ec6c64c810cf4e77b26065f26ed16
1xtend <kokitoki0504@gmail.com>
2022-06-25 13:33:14 +0200

    add-task-1

---
+++ Module-3/add-task-1.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const cart = {
+  products: [],
+  add(product) {
+    this.products.push(product);
+  },
+  remove(productName) {
+    for (let i = 0; i < this.products.length; i++) {
+      if (this.products[i].name === productName) {
+        this.products.splice(i, 1);
+        return;
+      }
+    }
+  },
+  clear() {
+    this.products = [];
+  },
+  getAll() {
+    return this.products;
+  },
+
+  getTotalPrice() {
+    let total = 0;
+
+    for (const product of this.products) {
+      total += product.price * product.quantity;
+    }
+
+    return total;
+  },
+};
+
+const apple = {
+  name: 'Яблуко',
+  price: 9.99,
+  quantity: 3,
+};
+
+const orange = {
+  name: 'Апельсин',
+  price: 12,
+  quantity: 1,
+};
+
+cart.add(apple);
+cart.add(orange);
+console.table(cart.getAll());
+// cart.remove('Апельсин');
+// cart.clear();
+
+// console.log(cart.getAll());
+
+console.log(cart.getTotalPrice());
